1f469713d806c2fe80b5de6607887174265abd3f
Alan Hylands <alan@alanhylands.com>
2019-09-09 13:08:44 +0100

    once again

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Twitter, LinkedIn } from '../components/Social'
+import { Twitter, LinkedIn } from '../components/social'
 
 
 class AboutIndex extends React.Component {
